150ec4bdabcce3dc6534edfa645cef7b60eb13cd
montehoover <monte.b.hoover@gmail.com>
2016-03-30 10:09:02 -0700

    added delete button, local storage, and sortable ui

--- js/script.js
+++ js/script.js
@@ -1,11 +1,38 @@
-var toDoList = []
+$(document).ready(function() {
 
-$('#new-item-form').submit(function(e) {
-    e.preventDefault();
-    toDoList.push($('#text-input-box').val());
-    $('#to-do-list-div').html('');
-    for(var i = 0; i < toDoList.length; i++) {
-        $('#to-do-list-div').append('<li>' + toDoList[i] + '</li>');
+    var toDoList = JSON.parse(localStorage.getItem('toDoList')) || [];
+
+     var displayItem = function(s) {
+        $('#to-do-list').append('<li class="ui-state-default">' + s 
+            + '<button class="delete ">Delete</button></li>');
     }
-    $(this)[0].reset();
+
+    // $('#to-do-list').html('');
+
+    for (var i = 0; i < toDoList.length; i++) {
+        displayItem(toDoList[i]);
+    }
+
+    $('#new-item-form').submit(function(e) {
+        e.preventDefault();
+        var x = $('#text-input-box').val();
+        if (x) {
+            toDoList.push(x)
+            localStorage.setItem('toDoList', JSON.stringify(toDoList));
+            displayItem(x);
+        }
+        $(this)[0].reset();
+    });
+
+    $(document).on('click', '.delete', function(e) {
+        $(this).parent().remove();
+        var i = toDoList.indexOf($(this).val());
+        toDoList.splice(i,1);
+        localStorage.setItem('toDoList', JSON.stringify(toDoList));
+    });
+
+    $(function() {
+        $('#to-do-list').sortable();    
+    });
+
 });
